6439032b22aec568110e991963929384089052ae
Jan Jurica <john.jurica@seznam.cz>
2024-01-23 08:14:21 +0100

    add about page with IPAdress, MACAdress and name of device

--- app/src/main/java/com/example/restapi/AboutActivity.kt
+++ app/src/main/java/com/example/restapi/AboutActivity.kt
@@ -2,7 +2,16 @@ package com.example.restapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Build
 import android.view.MenuItem
+import android.content.Context
+import android.net.wifi.WifiInfo
+import android.net.wifi.WifiManager
+import android.widget.TextView
+import java.net.NetworkInterface
+import java.net.Inet4Address
+import java.net.InetAddress
+import java.util.*
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +19,18 @@ class AboutActivity : AppCompatActivity() {
         setContentView(R.layout.activity_about)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true) // Povolit tlačítko zpět
+
+        val textViewMacAddress: TextView = findViewById(R.id.textViewMacAddress)
+        val textViewIpAddress: TextView = findViewById(R.id.textViewIpAddress)
+        val textViewDeviceName: TextView = findViewById(R.id.textViewDeviceName)
+
+        val macAddress = getMacAddress()
+        val ipAddress = getIpAddress()
+        val deviceName = getDeviceName()
+
+        textViewMacAddress.text = "MAC Address: $macAddress"
+        textViewIpAddress.text = "IP Address: $ipAddress"
+        textViewDeviceName.text = "Device Name: $deviceName"
 
     }
 
@@ -21,5 +42,60 @@ class AboutActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun getMacAddress(): String {
+        try {
+            val networkInterface: NetworkInterface? = NetworkInterface.getByName("wlan0")
+
+            if (networkInterface != null) {
+                val macAddressBytes: ByteArray? = networkInterface.hardwareAddress
+                if (macAddressBytes != null) {
+                    val macAddress = StringBuilder()
+                    for (b in macAddressBytes) {
+                        macAddress.append(String.format("%02X:", b))
+                    }
+                    if (macAddress.isNotEmpty()) {
+                        macAddress.deleteCharAt(macAddress.length - 1)
+                        return macAddress.toString()
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return "N/A"
+    }
+
+    private fun getIpAddress(): String {
+        var ipAddress = "N/A"
+        try {
+            val networkInterfaces: Enumeration<NetworkInterface> = NetworkInterface.getNetworkInterfaces()
+
+            while (networkInterfaces.hasMoreElements()) {
+                val networkInterface: NetworkInterface = networkInterfaces.nextElement()
+
+                val inetAddresses: Enumeration<InetAddress> = networkInterface.inetAddresses
+
+                while (inetAddresses.hasMoreElements()) {
+                    val inetAddress: InetAddress = inetAddresses.nextElement()
+
+                    // Příklad: Vracíte první nalezenou IP adresu
+                    if (!inetAddress.isLoopbackAddress && inetAddress is Inet4Address) {
+                        ipAddress = inetAddress.hostAddress
+                        break
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return ipAddress
+    }
+
+    private fun getDeviceName(): String {
+        return Build.MODEL
     }
 }
